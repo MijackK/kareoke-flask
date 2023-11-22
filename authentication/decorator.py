1972bbac8974712a2a6d5158b251380c1aee5356
Mijack <dr.mijack@gmail.com>
2023-11-23 00:52:32 +0300

    update

--- authentication/decorator.py
+++ authentication/decorator.py
@@ -3,39 +3,41 @@ from app import db
 from authentication.models import User
 from functools import wraps
 
+
 def login_required(callback):
     @wraps(callback)
     def decorated_function(**kwargs):
         if "user_id" in session:
             csrf_token = request.headers.get("CSRF_TOKEN")
-            if csrf_token == session['csrf_token']:
+            if csrf_token == session["csrf_token"]:
                 return callback(**kwargs)
             print("Cross site attack prevented")
             abort(401)
         print("Not allowed, please login")
         abort(401)
+
     return decorated_function
+
 
 def require_admin(callback):
     @wraps(callback)
     def decorated_function(**kwargs):
         if "user_id" in session:
-            is_admin = User.query.get(session['user_id']).admin
-            if(is_admin):
+            is_admin = User.query.get(session["user_id"]).admin
+            if is_admin:
                 return callback(**kwargs)
-        print("Only administrator is allowed")
         abort(403)
+
     return decorated_function
-            
+
+
 def require_verify(callback):
     @wraps(callback)
     def decorated_function(**kwargs):
         if "user_id" in session:
-            is_verified = User.query.get(session['user_id']).verify
-            if(is_verified):
+            is_verified = User.query.get(session["user_id"]).verify
+            if is_verified:
                 return callback(**kwargs)
-        print("Please verify your email first")
-        abort(403)
+        abort(403, "Email not verified")
+
     return decorated_function
-
-
