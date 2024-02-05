356751f007fd3dd3b21b738d6c4eef0765f76db2
Mijack <dr.mijack@gmail.com>
2024-02-05 17:28:35 +0300

    update

--- authentication/utility/email.py
+++ authentication/utility/email.py
@@ -1,9 +1,9 @@
 from flask_mail import Message
 from app import mail
-from flask import render_template
 
 
 def send_mail(recipient, html, title):
     msg = Message(title, recipients=[recipient])
     msg.html = html
+    print("reached here")
     mail.send(msg)
